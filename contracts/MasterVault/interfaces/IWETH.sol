288e443682c188f98fa7b93a471809dedca39294
akkiselbatsnu <xirexor.android@gmail.com>
2022-07-28 15:08:59 +0300

    Fixed Master Vault IERC20 Import

--- contracts/MasterVault/interfaces/IWETH.sol
+++ contracts/MasterVault/interfaces/IWETH.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-import "./IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 pragma solidity ^0.8.0;
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -24,6 +24,15 @@ module.exports = {
                     }
                 }
             },
+            {
+                version: '0.8.15',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 100
+                    }
+                }
+            },
         ]
     },
 
