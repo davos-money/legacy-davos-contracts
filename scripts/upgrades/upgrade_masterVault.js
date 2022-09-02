650761f1f80e53297dc136fed3e518ca8896869d
rohit2093 <rohitsethiii836@gmail.com>
2022-09-03 00:26:28 +0530

    Fixed: upgrades

--- scripts/upgrades/upgrade_masterVault.js
+++ scripts/upgrades/upgrade_masterVault.js
@@ -3,32 +3,32 @@ const {ethers, upgrades} = require("hardhat");
 const main = async () => {
 
   const admin_slot = "0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103";
-  const masterVaultProxy = "0x55E1260c0F2f58F0aAfc8e8D19293862A76B5ce9";
-  const cerosStrategyProxy = "0x0f57E801072B9b5b2b693849c7d8179E7B8410BE";
+  const masterVaultProxy = "0x764Ae6682DEB212a66f754dEe176901663C46554";
+  const cerosStrategyProxy = "0x55C9dd38733fd168d27F1ca68118515B2c6A29aE";
 
   const proxyAddress = await ethers.provider.getStorageAt(masterVaultProxy, admin_slot);
   const PROXY_ADMIN_ABI = ["function upgrade(address proxy, address implementation) public"]
   
-  this.MasterVault = await hre.ethers.getContractFactory("MasterVault");
-  let masterVaultImp = await this.MasterVault.deploy();
-  await masterVaultImp.deployed();
-  console.log("Master Vault Imp     : " + masterVaultImp.address);
+  // this.MasterVault = await hre.ethers.getContractFactory("MasterVault");
+  // let masterVaultImp = await this.MasterVault.deploy();
+  // await masterVaultImp.deployed();
+  // console.log("Master Vault Imp     : " + masterVaultImp.address);
 
-  this.CerosYieldConverterStrategy = await hre.ethers.getContractFactory("CerosYieldConverterStrategy");
-  let cerosStrImp = await this.CerosYieldConverterStrategy.deploy();
-  await cerosStrImp.deployed();
-  console.log("Master Vault Imp     : " + cerosStrImp.address);
+  // this.CerosYieldConverterStrategy = await hre.ethers.getContractFactory("CerosYieldConverterStrategy");
+  // let cerosStrImp = await this.CerosYieldConverterStrategy.deploy();
+  // await cerosStrImp.deployed();
+  // console.log("Master Vault Imp     : " + cerosStrImp.address);
 
   let proxyAdmin = await ethers.getContractAt(PROXY_ADMIN_ABI, parseAddress(proxyAddress));
-  await (await proxyAdmin.upgrade(masterVaultProxy, masterVaultImp.address)).wait();
-  await (await proxyAdmin.upgrade(cerosStrategyProxy, cerosStrImp.address)).wait();
+  await (await proxyAdmin.upgrade(masterVaultProxy, "0x99a5bc1524e1ceb75a205c3f02be9ac3ed042e63")).wait();
+  await (await proxyAdmin.upgrade(cerosStrategyProxy, "0x6DeF4570251E1f435E121b3Ee47174496D851C99")).wait();
   console.log("Upgraded Successfully...")
   
-  console.log("Verifying MasterVaultImp...")
-  await hre.run("verify:verify", {address: masterVaultImp.address});
+  // console.log("Verifying MasterVaultImp...")
+  // await hre.run("verify:verify", {address: masterVaultImp.address});
   
-  console.log("Verifying CerosStrategyImp...")
-  await hre.run("verify:verify", {address: cerosStrImp.address});
+  // console.log("Verifying CerosStrategyImp...")
+  // await hre.run("verify:verify", {address: cerosStrImp.address});
 };
 
 function parseAddress(addressString){
